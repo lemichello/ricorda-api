217e9b4060c3980a038ab132971a7f77292ac9be
Lemich Maksym <lemichello.m@gmail.com>
2020-03-28 13:01:44 +0200

    fix(controllers): added missing response messages

--- controllers/wordsController.js
+++ controllers/wordsController.js
@@ -13,7 +13,7 @@ const createPair = async (req, res) => {
     res.status(201).json({ data: wordsPair });
   } catch (e) {
     console.error(e);
-    res.status(500).end();
+    res.status(500).s1('Internal server error');
   }
 };
 
@@ -46,7 +46,7 @@ const updateWordsPair = async (req, res) => {
       .exec();
 
     if (!updatedDoc) {
-      return res.status(400).end();
+      return res.status(400).send('Received incorrect word pair');
     }
 
     res.status(200).json({ data: updatedDoc });
