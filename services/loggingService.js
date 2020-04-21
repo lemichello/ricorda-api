b0402f8ad1898769768806706a05e270c9947e0b
Lemich Maksym <lemichello.m@gmail.com>
2020-04-21 13:25:47 +0300

    feat(loggingService): added logging via console, when in dev mode

--- services/loggingService.js
+++ services/loggingService.js
@@ -2,25 +2,25 @@ const logger = require('../logger');
 
 const loggingService = {
   info: function (message, metaData) {
-    if (!logger) {
-      return;
+    if (logger) {
+      logger.info(message, { meta: metaData });
+    } else {
+      console.info(message);
     }
-
-    logger.info(message, { meta: metaData });
   },
   error: function (message, metaData) {
-    if (!logger) {
-      return;
+    if (logger) {
+      logger.error(message, { meta: metaData });
+    } else {
+      console.error(message);
     }
-
-    logger.error(message, { meta: metaData });
   },
   debug: function (message, metaData) {
-    if (!logger) {
-      return;
+    if (logger) {
+      logger.debug(message, { meta: metaData });
+    } else {
+      console.debug(message);
     }
-
-    logger.debug(message, { meta: metaData });
   },
 };
 
