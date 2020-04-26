d8c6c3b17ef02e60d83275dd3a9dd84901b0250f
Lemich Maksym <lemichello.m@gmail.com>
2020-04-26 20:06:20 +0300

    feat(*): added account router

--- app.js
+++ app.js
@@ -5,21 +5,30 @@ var logger = require('morgan');
 
 const authRouter = require('./routes/authRouter');
 const wordsRouter = require('./routes/wordsRouter');
+const accountRouter = require('./routes/accountRouter');
+
 const { protect } = require('./services/authService');
 const cors = require('cors');
 
 var app = express();
 
-const whitelist = ['https://ricorda-cfbe2.web.app', 'https://ricorda-stage.web.app', 'http://localhost:3001']
+const whitelist = [
+  'https://ricorda-cfbe2.web.app',
+  'https://ricorda-stage.web.app',
+  'http://localhost:3001',
+];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,5 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRouter);
 app.use('/api', protect);
 app.use('/api/words', wordsRouter);
+app.use('/api/account', accountRouter);
 
 module.exports = app;
---
+++ controllers/accountController.js
@@ -0,0 +1,66 @@
+const User = require('../models/userModel');
+const logger = require('../services/loggingService');
+
+const updatePassword = async (req, res) => {
+  let { oldPassword, newPassword } = req.body;
+
+  if (typeof oldPassword !== 'string' || typeof newPassword !== 'string') {
+    return res.status(400).send('Incorrect type of old or new passwords');
+  }
+
+  let user = await User.findById(req.user._id);
+  let correctOldPassword = await user.checkPassword(oldPassword);
+
+  if (!correctOldPassword) {
+    return res.status(400).send('Incorrect old password');
+  }
+
+  try {
+    user.password = newPassword;
+    user.save();
+
+    logger.info(`Changed password for user: ${req.user._id}`);
+    res.status(200).send('Successfully updated password');
+  } catch (e) {
+    logger.error(`Can't update password for user: ${e}`, {
+      userId: req.user._id,
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+    });
+    res.status(500).send('Internal server error');
+  }
+};
+
+const updateEmail = async (req, res) => {
+  let { newEmail } = req.body;
+
+  if (typeof newEmail !== 'string') {
+    return res.status(400).send('Incorrect type of new email');
+  }
+
+  try {
+    await User.findOneAndUpdate(
+      {
+        _id: req.user._id,
+      },
+      { email: newEmail },
+      { new: true, useFindAndModify: false }
+    )
+      .lean()
+      .exec();
+
+    logger.info(`Changed email for user: ${req.user._id}`);
+    res.status(200).send('Successfully updated email');
+  } catch (e) {
+    logger.error(`Can't update email for user: ${e}`, {
+      userId: req.user._id,
+      newEmail: newEmail,
+    });
+    res.status(500).send('Internal server error');
+  }
+};
+
+module.exports = {
+  updatePassword,
+  updateEmail,
+};
